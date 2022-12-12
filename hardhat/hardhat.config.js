67b33ccf13754bae90714e7ce986dd44359b4a9e
santipu <santipujolsau@gmail.com>
2022-12-12 17:24:30 +0100

    modified hh config file to deploy to Mumbai testnet

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -10,6 +10,8 @@ const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL
+const POLYGONSCAN_KEY = process.env.POLYGONSCAN_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -29,6 +31,10 @@ module.exports = {
         localhost: {
             chainId: 31337,
         },
+        mumbai: {
+            url: MUMBAI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+        },
     },
     namedAccounts: {
         deployer: {
@@ -39,7 +45,10 @@ module.exports = {
         },
     },
     etherscan: {
-        apiKey: ETHERSCAN_API_KEY,
+        apiKey: {
+            polygonMumbai: POLYGONSCAN_KEY,
+            goerli: ETHERSCAN_API_KEY,
+        },
     },
     gasReporter: {
         enabled: false,
